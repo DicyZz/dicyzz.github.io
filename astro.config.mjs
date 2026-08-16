d83311c7da88a8c51d7504661aa4075640178e65
Jan Zhang <jianzhang0248@gmail.com>
2026-08-16 17:11:37 +0800

    Add Unocss and astro-icon integrations to config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,16 @@
 import { defineConfig } from 'astro/config';
+import Unocss from 'unocss/astro';
+import astroIcon from 'astro-icon';
 import { CUSTOM_DOMAIN, BASE_PATH } from './src/server-constants.ts';
 
 // https://astro.build/config
 export default defineConfig({
   site: CUSTOM_DOMAIN || 'https://zhangjian0248.top',
   base: BASE_PATH || '/blog',
-  integrations: [],
+  integrations: [
+    Unocss({
+      injectReset: true,
+    }),
+    astroIcon(),
+  ],
 });
