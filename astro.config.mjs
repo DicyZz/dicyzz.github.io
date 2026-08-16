ecfa664261f93824cc769ef7c16ac26ffce26730
Jan Zhang <jianzhang0248@gmail.com>
2026-08-16 17:09:10 +0800

    Refactor astro.config.mjs for site and base settings

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,46 +1,9 @@
 import { defineConfig } from 'astro/config';
-import icon from 'astro-icon';
-import { CUSTOM_DOMAIN, BASE_PATH } from './src/server-constants';
-import CoverImageDownloader from './src/integrations/cover-image-downloader';
-import CustomIconDownloader from './src/integrations/custom-icon-downloader';
-import FeaturedImageDownloader from './src/integrations/featured-image-downloader';
-import PublicNotionCopier from './src/integrations/public-notion-copier';
-
-const getSite = function () {
-  if (CUSTOM_DOMAIN) {
-    return new URL(BASE_PATH, `https://${CUSTOM_DOMAIN}`).toString();
-  }
-
-  if (process.env.VERCEL && process.env.VERCEL_URL) {
-    return new URL(BASE_PATH, `https://${process.env.VERCEL_URL}`).toString();
-  }
-
-  if (process.env.CF_PAGES) {
-    if (process.env.CF_PAGES_BRANCH !== 'main') {
-      return new URL(BASE_PATH, process.env.CF_PAGES_URL).toString();
-    }
-
-    return new URL(
-      BASE_PATH,
-      `https://${new URL(process.env.CF_PAGES_URL).host
-        .split('.')
-        .slice(1)
-        .join('.')}`
-    ).toString();
-  }
-
-  return new URL(BASE_PATH, 'http://localhost:4321').toString();
-};
+import { CUSTOM_DOMAIN, BASE_PATH } from './src/server-constants.ts';
 
 // https://astro.build/config
 export default defineConfig({
-  site: getSite(),
-  base: ‘/blog’,
-  integrations: [
-    icon(),
-    CoverImageDownloader(),
-    CustomIconDownloader(),
-    FeaturedImageDownloader(),
-    PublicNotionCopier(),
-  ],
+  site: CUSTOM_DOMAIN || 'https://zhangjian0248.top',
+  base: BASE_PATH || '/blog',
+  integrations: [],
 });
