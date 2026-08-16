537f598f4751a4e3aa8f1beee883b4c14fe49270
Jan Zhang <jianzhang0248@gmail.com>
2026-08-16 17:14:41 +0800

    Add new image downloader integrations to config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,18 @@
 import { defineConfig } from 'astro/config';
-import Unocss from 'unocss/astro';
-import astroIcon from 'astro-icon';
 import { CUSTOM_DOMAIN, BASE_PATH } from './src/server-constants.ts';
+import CoverImageDownloader from './src/integrations/cover-image-downloader';
+import CustomIconDownloader from './src/integrations/custom-icon-downloader';
+import FeaturedImageDownloader from './src/integrations/featured-image-downloader';
+import PublicNotionCopier from './src/integrations/public-notion-copier';
 
 // https://astro.build/config
 export default defineConfig({
   site: CUSTOM_DOMAIN || 'https://zhangjian0248.top',
   base: BASE_PATH || '/blog',
   integrations: [
-    Unocss({
-      injectReset: true,
-    }),
-    astroIcon(),
+    CoverImageDownloader(),
+    CustomIconDownloader(),
+    FeaturedImageDownloader(),
+    PublicNotionCopier(),
   ],
 });
